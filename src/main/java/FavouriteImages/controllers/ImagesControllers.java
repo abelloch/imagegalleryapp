4df6debc8c1e01bc5220cf78ee472b7ff378b615
Abelloch <abelloch.es@gmail.com>
2024-07-09 15:24:41 +0200

    feature Update by Id Done

--- src/main/java/FavouriteImages/controllers/ImagesControllers.java
+++ src/main/java/FavouriteImages/controllers/ImagesControllers.java
@@ -1,16 +1,49 @@
 package FavouriteImages.controllers;
 
 
+import FavouriteImages.models.Images;
+import FavouriteImages.repositories.IImagesRepository;
 import FavouriteImages.services.ImagesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
-@CrossOrigin
+@RequestMapping("/api/v1")
+@CrossOrigin ( origins = "*")
+
 public class ImagesControllers {
     @Autowired
     ImagesService imagesService;
+
+    @Autowired
+    private IImagesRepository imagesRepository;
+
+
+    // Endpoint para crear una nueva imagen
+    @PostMapping(path = "/images")
+    public ResponseEntity<String> createImage(@RequestBody Images image) {
+        return imagesService.createImage(image);
+    }
+    // Endpoint para actualizar una nueva imagen por id
+    @PutMapping(path = "/images/{id}")
+    public ResponseEntity<String> updateImage(@PathVariable int id, @RequestBody Images newImageDetails) {
+        return imagesService.updateImage(newImageDetails, id);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/FavouriteImages/services/ImagesService.java
+++ src/main/java/FavouriteImages/services/ImagesService.java
@@ -1,12 +1,51 @@
 package FavouriteImages.services;
 
+import FavouriteImages.models.Images;
 import FavouriteImages.repositories.IImagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
 
 @Service
 public class ImagesService {
+
     @Autowired
     IImagesRepository iImagesRepository;
 
+    // Método para crear una nueva imagen
+    public ResponseEntity<String> createImage(Images image) {
+        try {
+            iImagesRepository.save(image);
+            return new ResponseEntity<>("Image created successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while creating the image", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Update by ID
+    public ResponseEntity<String> updateImage(Images newImageDetails, int id) {
+        try {
+            Optional<Images> optionalImage = iImagesRepository.findById(id);
+            if (optionalImage.isPresent()) {
+                Images existingImage = optionalImage.get();
+                existingImage.setTitle(newImageDetails.getTitle());
+                existingImage.setUrl(newImageDetails.getUrl());
+                existingImage.setDescription(newImageDetails.getDescription());
+                iImagesRepository.save(existingImage);
+                return new ResponseEntity<>("Image updated successfully", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Image not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while updating the image", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
+
+
