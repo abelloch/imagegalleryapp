1cd3769e16a38113a8293b4770a7d24d03a9bfaa
Hilmar09 <hil.cris09@gmail.com>
2024-07-10 10:41:47 +0200

    testing delete

--- src/main/java/FavouriteImages/controllers/ImagesControllers.java
+++ src/main/java/FavouriteImages/controllers/ImagesControllers.java
@@ -1,28 +1,29 @@
 package FavouriteImages.controllers;
 
 
+import FavouriteImages.models.Images;
 import FavouriteImages.services.ImagesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
-public class ImagesControllers<id> {
-
+public class ImagesControllers {
     @Autowired
     ImagesService imagesService;
 
     @DeleteMapping(path = "/{id}")
-    public String deleteImagesById(@PathVariable int id){
+    public String deleteImagesById(@PathVariable int id) {
 
         boolean ok = imagesService.deleteImageById(id);
         if (ok) {
             return "image with id" + id + "as deleted";
         } else {
-            return  "Error, we have a problem to deleted image" + id;
+            return "Error, we have a problem to deleted image" + id;
         }
 
+
     }
-}
