4feffc36e8aea4ffa4c33f01542acd1888e664a6
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-10 10:36:09 +0200

    merge stasjs¡h

--- src/main/java/FavouriteImages/controllers/ImagesControllers.java
+++ src/main/java/FavouriteImages/controllers/ImagesControllers.java
@@ -6,6 +6,8 @@ import FavouriteImages.services.ImagesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
@@ -17,4 +19,24 @@ public class ImagesControllers {
     public Images createImages(@RequestBody Images newImages){
         return imagesService.createImages(newImages);
     }
+
+  @DeleteMapping(path = "images/{id}")
+  public String deleteImagesById(@PathVariable int id) {
+    boolean ok = imagesService.deleteImagesById(id);
+    if (ok) {
+      return "image with id" + id + "as deleted";
+    } else {
+      return "Error, we have a problem to deleted image" + id;
+    }
+}
+    @GetMapping(path = "")
+    public ArrayList<Images> getAllImages() {
+        return imagesService.getAllImages();
+    }
+
+    @GetMapping(path = "/images/{id}")
+    public Images getImageId(@PathVariable int id) {
+        return imagesService.getImageId(id);
+        }
+
 }
---
+++ src/test/java/FavouriteImages/services/ImagesServiceTest.java
@@ -0,0 +1,81 @@
+package FavouriteImages.services;
+
+import FavouriteImages.models.Images;
+import FavouriteImages.repositories.IImagesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class ImagesServiceTest {
+    @Mock
+    private IImagesRepository iImagesRepository;
+
+    @InjectMocks
+    private ImagesService imagesService;
+
+    private Images imagePersona;
+    private Images imageGrandblue;
+    private ArrayList<Images> imagesList = new ArrayList<>();
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        imagePersona = new Images();
+        imagePersona.setId(1);
+        imagePersona.setTitle("Persona");
+        imagePersona.setDescription("Game persona 4");
+        imagePersona.setUrl("https://miro.medium.com/v2/resize:fit:1400/1*cz97DqG8yiPh2G2k3lz-rQ.jpeg");
+
+        imageGrandblue = new Images();
+        imageGrandblue.setId(2);
+        imageGrandblue.setTitle("Granblue Fantasy");
+        imageGrandblue.setDescription("Game Granblue Fantasy Relink");
+        imageGrandblue.setUrl("https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/01/granblue-fantasy-relink-key-art.jpg");
+
+        imagesList.add(imagePersona);
+        imagesList.add(imageGrandblue);
+    }
+    @Test
+    void getAllImages() {
+        when(imagesService.getAllImages()).thenReturn(imagesList);
+
+        ArrayList<Images> allImages = imagesService.getAllImages();
+        assertEquals(2, allImages.size());
+        assertEquals("Persona", allImages.get(0).getTitle());
+        assertEquals("Granblue Fantasy", allImages.get(1).getTitle());
+    }
+
+    @Test
+    void getImageId() {
+        when(iImagesRepository.findById(2)).thenReturn(Optional.of(imageGrandblue));
+        Images imageId = imagesService.getImageId(2);
+        assertEquals("Granblue Fantasy", imageId.getTitle());
+        assertEquals("Game Granblue Fantasy Relink", imageId.getDescription());
+    }
+    @Test
+    void createImages() {
+        when(iImagesRepository.save(any(Images.class))).thenReturn(imageGrandblue);
+        Images newImages = imagesService.createImages(imageGrandblue);
+
+        assertNotNull(newImages);
+        assertEquals(2,newImages.getId());
+        assertEquals("Granblue Fantasy", newImages.getTitle());
+        assertEquals("Game Granblue Fantasy Relink", newImages.getDescription());
+        assertEquals("https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/01/granblue-fantasy-relink-key-art.jpg", newImages.getUrl());
+    }
+
+}
