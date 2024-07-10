6d0452e83c06bd98cf3b0784ed66a72069a7f5a5
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-10 10:40:19 +0200

    changes any

--- src/test/java/FavouriteImages/services/ImagesServiceTest.java
+++ src/test/java/FavouriteImages/services/ImagesServiceTest.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class ImagesServiceTest {
