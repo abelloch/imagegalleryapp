d69c2e24782f69ac239aa91a566f831e0f9b5ec8
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-15 09:12:59 +0200

    stash

--- src/main/java/FavouriteImages/services/ImagesService.java
+++ src/main/java/FavouriteImages/services/ImagesService.java
@@ -7,49 +7,50 @@ import org.springframework.boot.context.config.ConfigDataResourceNotFoundExcepti
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 
-
 import java.util.Collections;
 import java.util.Optional;
 
 @Service
 public class ImagesService {
 
-    @Autowired
-    IImagesRepository iImagesRepository;
+  @Autowired IImagesRepository iImagesRepository;
 
-    public Images createImages (Images newimages){
-        return iImagesRepository.save(newimages);
-    }
-    public boolean deleteImagesById(int id) {
-        try {
-            iImagesRepository.deleteById(id);
-            return true;
-        }catch (Exception e){
-            return false;
-        }
-    }
+  public Images createImages(Images newimages) {
+    return iImagesRepository.save(newimages);
+  }
 
-    public ArrayList<Images> getAllImages() {
-        return (ArrayList<Images>) iImagesRepository.findAll();
+  public boolean deleteImagesById(int id) {
+    try {
+      iImagesRepository.deleteById(id);
+      return true;
+    } catch (Exception e) {
+      return false;
     }
+  }
 
-    public Images getImageId(int id) {
-        Images image = iImagesRepository.findById(id).orElseThrow();;
-        return image;
-    }
+  public ArrayList<Images> getAllImages() {
+    return (ArrayList<Images>) iImagesRepository.findAll();
+  }
 
-    public void updateImage(Images images, Integer id) {
-        images.setId(id);
-        iImagesRepository.save(images);
-    }
-           public Images updateFavorite(int id, boolean favorite) {
-               return iImagesRepository.findById(id).map(image -> {
-                   image.setFavorite(favorite);
-                   return iImagesRepository.save(image);
-               }).orElseThrow(() -> new IllegalArgumentException("Image not found with id " + id));
-           }
+  public Images getImageId(int id) {
+    Images image = iImagesRepository.findById(id).orElseThrow();
+    ;
+    return image;
+  }
+
+  public void updateImage(Images images, Integer id) {
+    images.setId(id);
+    iImagesRepository.save(images);
+  }
+
+  public Images updateFavorite(int id, boolean favorite) {
+    return iImagesRepository
+        .findById(id)
+        .map(
+            image -> {
+              image.setFavorite(favorite);
+              return iImagesRepository.save(image);
+            })
+        .orElseThrow(() -> new IllegalArgumentException("Image not found with id " + id));
+  }
 }
-
-
-
-
